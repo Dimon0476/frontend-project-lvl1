ed2dcc6cd13c0e069f15917e51c48a4576394d6f
Dimon0476 <dmitry.fodorov@gmail.com>
2024-07-23 22:13:51 +0300

    Step_4

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 
 function greetings() {
-    return console.log('Welcome to the Brain Games!');
+  return console.log('Welcome to the Brain Games!');
 }
 
 function name(question = 'May I have your name? ') {
-    var userName = readlineSync.question(question);
-    console.log('Hi ' + userName + '!');
+  var userName = readlineSync.question(question);
+  console.log('Hi ' + userName + '!');
 }
 
 export { name, greetings };
